700def740eca1269adb5a9fd5449c0761502d9c4
Tre Landaiche <trebug123@gmail.com>
2024-11-21 11:20:08 -0600

    update parsecsv to not use timestamps from original dataset

--- parsecsv.py
+++ parsecsv.py
@@ -4,14 +4,17 @@ import csv
 
 with open('training.1600000.processed.noemoticon.csv', 'r') as file:
 	reader = csv.reader(file)
+	i = 0
 	for row in reader:
 		# This is the data we want to save
-		# timestamp (uuid), username, post content
-		data = row[1], row[4], row[5]
+		# tweet number, username, post content
+		# Cannot use the timestamp because there are duplicates
+		data = i, row[4], row[5]
 		# print(data)
 		# Save dato to a new CSV file
 		with open('tweets.csv', 'a') as newfile:
 			writer = csv.writer(newfile)
 			writer.writerow(data)
+		i += 1
 	newfile.close()
 		
